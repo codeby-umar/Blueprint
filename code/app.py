43bdc20edab557c496a995a25b74556110e49f2b
codeby-umar <codingbyumar@gmail.com>
2026-02-26 08:37:36 +0500

    misol

---
+++ code/app.py
@@ -0,0 +1,38 @@
+import math
+# sqrt - ildizga chiqarib beradi
+# pow - darajaga kotaradi
+# fabs - modul manfiy bolsa musbat qilib beradi
+# log - narural lagarifm
+# max - Saralab beradi sort qilib beradi
+# min - Saralab beradi pastga qarab
+# pi - o'zgarmas qiymat constanta
+# e - logarifm bu ham qonstanta
+
+
+# a = int(input("Soz kiriting :"))
+
+# if a > 100:
+#     s = math.sqrt(a)
+#     print("Ildizi :" , s)
+# else:
+#     s = math.pow(a , 3)
+#     print("Kvadrati :" , s)
+ 
+
+# a = int(input("Son kiritng :"))
+
+# if a / 2:
+#     print("ildizi :" , math.sqrt(a))
+# else:
+#     print("Darajaga ko'tarish :" , math.pow(a , 3))
+
+
+# a = int(input("Son kiriting :"))
+# b = int(input("Son kiriting :"))
+
+# if a > b:
+#     print(a + b)
+# elif b > a:
+#     print(a - b)
+# else:
+#     print("Xatolik yuz berdi")
