261a326137cd53d4a725b090a2f4de25eaebfe5b
Muhammad Umar <shaxriyorturgunov000@gmail.com>
2026-05-09 19:31:53 +0500

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,29 @@
+def qoshish(a , b):
+    return a + b
+
+def qoshish(a , b):
+    return a - b
+
+def qoshish(a , b):
+    return a * b
+
+def qoshish(a , b):
+    return a / b
+
+
+def pow(a , b):
+    return a ** b
+
+
+def present(a , b):
+    return (a * b) / 100
+
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+
